64bad77083b2692c2779d42f401c7a208a721e87
ShahOdin <ShahOdin@users.noreply.github.com>
2017-06-27 13:31:14 +0100

    removed debug comments.

--- src/main/scala/ReporterActor.scala
+++ src/main/scala/ReporterActor.scala
@@ -14,17 +14,14 @@ case object LetItFlow
 
 class ReporterActor(snapshotFrequency:Int) extends PersistentActor {
 
-  override def persistenceId = "accountReporter8"
+  override def persistenceId = "AccountReporter"
 
-  //events.map(elem => self ! elem).runWith(Sink.ignore)
-  //events.runWith(Sink.actorRef(self, None))
 
   var balanceCache = 0f
   var journalEventOffset= 0L
 
-  def bookKeeping()={
+  def bookKeeping(): Unit ={
     journalEventOffset+=1
-    println(s"after operation, cache: $balanceCache , offset: $journalEventOffset")
     if (journalEventOffset % snapshotFrequency == 0)
       self ! requestSnapshot
   }
@@ -40,28 +37,20 @@ class ReporterActor(snapshotFrequency:Int) extends PersistentActor {
       case RejectedTransaction(_, _, _) ⇒ //nothing
     }
     bookKeeping()
+    println(s"after operation, cache: $balanceCache , offset: $journalEventOffset")
   }
 
   val receiveRecover: Receive = {
 
     case evt: DomainEvent =>
-      println(s"Reporter received $evt on recovering mode")
       updateCache(evt)
 
     case SnapshotOffer(_, ReporterCache(cache_, offset_)) =>
-      println(s"Counter receive snapshot with data: cache: $cache_ and offset: $offset_ on recovering mood")
       balanceCache= cache_
       journalEventOffset= offset_
-
-    case RecoveryCompleted =>
-      println(s"Recovery Complete and Now the reporter Class will switch to receiving mode")
   }
 
   val receiveCommand: Receive = {
-    case SaveSnapshotSuccess(_) =>
-      println(s"save snapshot succeed, with data: cache: $balanceCache and offset: $journalEventOffset ")
-    case SaveSnapshotFailure(_, reason) =>
-      println(s"save snapshot failed and failure is $reason")
 
     case `requestSnapshot`⇒
       saveSnapshot(
@@ -69,24 +58,18 @@ class ReporterActor(snapshotFrequency:Int) extends PersistentActor {
       )
 
     case `LetItFlow` ⇒
-
-      println(s"Let the stream take us all: cache: $balanceCache and offset: $journalEventOffset")
       val queries = PersistenceQuery(context.system).
         readJournalFor[LeveldbReadJournal](LeveldbReadJournal.Identifier)
       lazy val events = queries.eventsByPersistenceId(Account.persistenceId,journalEventOffset)
       implicit val materializer = ActorMaterializer()
-      //events.map(elem => self ! elem).runWith(Sink.ignore)
       events.runWith(Sink.actorRef(self, None))
 
-    //the singleton's events
-    case EventEnvelope(offset,_,_,evt:DomainEvent) ⇒
-      println(s"Reporter received the account's command: $evt with offset: $offset")
+    case EventEnvelope(_,_,_,evt:DomainEvent) ⇒
       persist(evt) { evt =>
         updateCache(evt)
       }
-    //internal events
-    case EventEnvelope(_,_,_,_) ⇒
-      //bookKeeping()
+
+    case EventEnvelope(_,_,_,_) ⇒ //nothing?
 
     case evt ⇒ println(s"didn't: $evt")
   }
