4c1a517b353e88f093f04cb0e9e1fee6f26c2d1b
ShahOdin <ShahOdin@users.noreply.github.com>
2017-07-03 15:43:19 +0100

    -changed to class instead of mixin.

--- src/main/scala/com/shah/persistence/demo/AccountData.scala
+++
@@ -1,8 +0,0 @@
-package com.shah.persistence.demo
-
-import com.shah.persistence.query.model.QueryViewData
-
-case class AccountData( override var cache: Float,
-                        override var offsetForNextFetch: Long= 1L) extends QueryViewData{
-  type Data = Float
-}
--- src/main/scala/com/shah/persistence/demo/AccountView.scala
+++ src/main/scala/com/shah/persistence/demo/AccountView.scala
@@ -3,6 +3,7 @@ package com.shah.persistence.demo
 import akka.actor.Props
 import akka.persistence.PersistentActor
 import com.shah.persistence.demo.Account._
+import com.shah.persistence.demo.AccountView.API
 import com.shah.persistence.query.model.{LeveldBQuerySupport, QueryView}
 
 import scala.concurrent.ExecutionContext
@@ -12,47 +13,43 @@ object AccountViewApi{
   case object ReadAccountBalance
 }
 
-class AccountView
+class AccountView(override val snapshotFrequency: Int)
+                 (implicit override val data: ClassTag[Float],
+                  override val ec: ExecutionContext)
+  extends PersistentActor() with QueryView[Float] with LeveldBQuerySupport {
 
+  override def viewId: String = AccountView.identifier
+  override def queryId: String = Account.identifier
+
+  override var cachedData: Float = 0L
+
+  def handleReads: Receive ={
+    case API.ReadAccountBalance ⇒
+      println(s"Account balance: $cachedData")
+  }
+
+  def updateCache: Receive ={
+    case AcceptedTransaction(amount, CR) ⇒
+      cachedData += amount
+      println(s"+Read  side balance: $cachedData")
+    case AcceptedTransaction(amount, DR) ⇒
+      val newAmount = cachedData - amount
+      if (newAmount > 0)
+        cachedData = newAmount
+      println(s"-Read  side balance: $cachedData")
+
+    case RejectedTransaction(_, _, _) ⇒ //nothing
+  }
+
+  override def receiveCommand: Receive = handleReads orElse updateCache
+}
 
 object AccountView {
   val API= AccountViewApi
-  def props(_snapshotFrequency: Int)
-           (implicit dataCT: ClassTag[Float],
-            _ec: ExecutionContext)
-    = Props(
-    new PersistentActor with QueryView[Float] with LeveldBQuerySupport {
-
-      override def viewId: String = AccountView.identifier
-      override def queryId: String = Account.identifier
-
-      override val snapshotFrequency: Int = _snapshotFrequency
-      override var cachedData: Float = 0L
-
-      def handleReads: Receive ={
-        case API.ReadAccountBalance ⇒
-          println(s"Account balance: $cachedData")
-      }
-
-      def updateCache: Receive ={
-        case AcceptedTransaction(amount, CR) ⇒
-          cachedData += amount
-          println(s"+Read  side balance: $cachedData")
-        case AcceptedTransaction(amount, DR) ⇒
-          val newAmount = cachedData - amount
-          if (newAmount > 0)
-            cachedData = newAmount
-          println(s"-Read  side balance: $cachedData")
-
-        case RejectedTransaction(_, _, _) ⇒ //nothing
-      }
-
-      override def receiveCommand: Receive = handleReads orElse updateCache
-
-      override implicit val ec: ExecutionContext = _ec
-      override implicit val data = dataCT
-    }
-  )
+  def props(snapshotFrequency: Int)
+           (implicit data: ClassTag[Float],
+            ec: ExecutionContext)
+  =  Props(new AccountView(snapshotFrequency))
 
   val identifier: String = "ReporterActor"
 }
